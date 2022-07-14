1700dec97c3e415e005a4b99c0254f46ca09c235
YuliaKokorieva <yuliya.pastushenko@gmail.com>
2022-07-14 11:45:06 +0300

    ex 13.21

--- controllers/users.js
+++ controllers/users.js
@@ -29,7 +29,7 @@ router.get('/:id', async (req, res) => {
         as: 'readings',
         attributes: {exclude: ['userId', 'createdAt', 'updatedAt']},
         through: {
-          attributes: []
+          attributes: ['id','read']
         }
       },
       attributes: {
