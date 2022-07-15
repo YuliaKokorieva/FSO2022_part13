051e059993697c3a93e6be1b4968e644cf21a44c
YuliaKokorieva <yuliya.pastushenko@gmail.com>
2022-07-15 11:02:43 +0300

    ex 13.22

--- controllers/readinglists.js
+++ controllers/readinglists.js
@@ -1,5 +1,11 @@
 const router = require('express').Router()
-const {ReadingList} = require('../models')
+const {ReadingList, User} = require('../models')
+const { tokenExtractor } = require('../util/middleware')
+
+const readinglistitemFinder = async (req,res,next) => {
+  req.readinglistitem = await ReadingList.findByPk(req.params.id)
+  next()
+}
 
 router.post('/', async (req, res, next) => {
   try{
@@ -7,6 +13,27 @@ router.post('/', async (req, res, next) => {
     res.json(item)
   } catch(error) {
     next(error)
+  }
+})
+
+router.get('/', async(req,res,next) => {
+  const readinglists = await ReadingList.findAll()
+  res.json(readinglists)
+})
+
+router.put('/:id', tokenExtractor, readinglistitemFinder, async(req,res,next) => {
+  const user = await User.findByPk(req.decodedToken.id)
+  if (req.readinglistitem) {
+    if (user.id === req.readinglistitem.userId) {
+      req.readinglistitem.read = req.body.read
+      await req.readinglistitem.save()
+      res.json(req.readinglistitem)
+
+    } else {
+      res.status(401).send("You don't have permission to modify this readinglist item")
+    }
+  } else {
+    res.status(404).end()
   }
 })
 
